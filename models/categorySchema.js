083d0a04ff9f858e1f3d96c4933658ce2aee9dd8
AfsalKP-007 <afsalbrototype007@gmail.com>
2025-03-30 17:54:27 +0530

    model folder attched.

---
+++ models/categorySchema.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  isListed: {
+    type: Boolean,
+    default: true,
+  },
+  offer: {
+    type: Number,
+    default: 0,
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Category = mongoose.model("Category", categorySchema);
+module.exports = Category;
---
+++ models/productSchema.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    salePrice: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    photos: {
+        type: [String], // Defines an array of strings
+        required: true,
+      },      
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    status: {
+      type: String,
+      enum: ["Available", "Out of stock", "Discountinued"],
+      required: true,
+      default: "Available",
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
